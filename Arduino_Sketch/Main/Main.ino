e070c5eca9a4a750f2889ec8b9f174a56ec633fb
csuarez689 <csuarez689@gmail.com>
2019-07-23 20:07:22 -0300

    Agregada configuracion BLE

--- Arduino_Sketch/Main/Main.ino
+++ Arduino_Sketch/Main/Main.ino
@@ -1,15 +1,28 @@
 #include "ArduinoNvs.h"
 #include "WiFi.h"
+#include "BluetoothSerial.h"
 
-String stMail, stUserPwd, stWifiName, stWifiPwd;
+BluetoothSerial SerialBT;
+
+// ASIGNACION DE PINES
+const int pinConfig = 36;
 
 void setup() {
   Serial.begin(115200);
   NVS.begin();
-  
-  //leer datos
-  //bluetoth
-  
+  // CONFIGURAR PINES COMO ENTRADAS
+  pinMode(pinConfig, INPUT);
+  Serial.println(digitalRead(pinConfig));
+  if(!digitalRead(pinConfig)){
+    Serial.println("Entrando en modo configuracion...");
+    conexionBT();
+    
+  }
+  Serial.println("Iniciando...");
+
+
+  Serial.println();
+  if (NVS.getString("stWifiName").c_str()=="Depto11"){Serial.println("==");}else{Serial.println("!=");}
   WiFi.mode(WIFI_STA);
   WiFi.begin(NVS.getString("stWifiName").c_str(),NVS.getString("stWifiPwd").c_str()); //Se leen WifiName y WifiPwd de la EEPROM
   while (WiFi.status() != WL_CONNECTED) {
@@ -30,25 +43,32 @@ void loop() {
     
 }
 
-//lectura de datos de configuracion
-void configurarSettings(){
-  //lectura de datos
-  Serial.print("\n Ingrese su correo: ");
-  Serial.setTimeout(8000);
-  stMail=Serial.readString();
-  Serial.print("\n Ingrese su contraseña de usuario: ");
-  Serial.setTimeout(8000);
-  stUserPwd=Serial.readString();
-  Serial.print("\n Ingrese el nombre de su red: ");
-  Serial.setTimeout(8000);
-  stWifiName=Serial.readString();
-  Serial.print("\n Ingrese la contraseña de su red: ");
-  Serial.setTimeout(8000);
-  stWifiPwd=Serial.readString();
+
+
+void conexionBT(){
+  uint8_t cont=0;
+  String st[4]={"_","_","_","_"};
+  String msj[4]={"Ingrese correo: ","Ingrese contraseña: ","Ingrese nombre de red: ","Ingrese contraseña de red: "};
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("Conecte su dispositivo");
+  delay(10000);
+  while(cont<4){
+    SerialBT.println(msj[cont]);
+    while(st[cont]=="_"){
+      if (SerialBT.available()) {
+        st[cont]=SerialBT.readStringUntil('\0');
+      }
+    }
+    cont++;
+  }
+  guardarSettings(st[0],st[1],st[2],st[3]);
+  Serial.println("Exito al configurar!");ESP.restart();
 }
 
+
+
 //guardado datos a eeprom
-void guardarSettings(){
+void guardarSettings(String stMail, String stUserPwd, String stWifiName, String stWifiPwd){
   if(!NVS.setString("stMail", stMail)){Serial.print("Error al guardar correo. Reiniciando...");delay(10000);ESP.restart();} //Correo electronico
   if(!NVS.setString("stUserPwd", stUserPwd)){Serial.print("Error al guardar contraseña usuario. Reiniciando...");delay(10000);ESP.restart();} //Contraseña de usuario
   if(!NVS.setString("stWifiName", stWifiName)){Serial.print("Error al guardar nombre de la red. Reiniciando...");delay(10000);ESP.restart();} //Nombre Wifi
